7ebcc3c78ff1ac786ac5028ef95516237161932d
SteDor <136347112+SteDor@users.noreply.github.com>
2023-10-02 13:41:32 +0200

    Add files via upload

---
+++ 02.10.2023_Php/Kurs.php
@@ -0,0 +1,24 @@
+<?php
+    $title = 'Kurs 02.10.2023';
+
+    include('./../include/header.php');
+    require('./../include/functions.php');
+
+    
+
+?>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h1 class="mt-5"></h1>
+        </div>
+      </div>
+      <h1>Kurs</h1>
+      <div class="row">
+        <?php            
+            
+        ?>
+      </div>
+    </div>
+  <?php include('./../include/footer.php');?>
